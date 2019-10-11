fe535c26946c2a236d38ceaf16f47c38f8e1b695
Edgar Diaz <diaz_eda@hotmail.com>
2019-10-11 16:13:07 -0500

    Agregando configuración FireBase.js

---
+++ app/utils/FireBase.js
@@ -0,0 +1,10 @@
+export default firebaseConfig = {
+  apiKey: "AIzaSyBeSyuLUh9TTgM9M2MVaJjeuc1AKI9O32w",
+  authDomain: "react-native-462a8.firebaseapp.com",
+  databaseURL: "https://react-native-462a8.firebaseio.com",
+  projectId: "react-native-462a8",
+  storageBucket: "react-native-462a8.appspot.com",
+  messagingSenderId: "140773570335",
+  appId: "1:140773570335:web:20b20278dbd662c2b8622a",
+  measurementId: "G-J5135C01VH"
+};

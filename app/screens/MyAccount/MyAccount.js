49f8d4e9584b973a5a18fc986641ff2ffbaaaa4d
Edgar Diaz <diaz_eda@hotmail.com>
2019-10-12 18:08:10 -0500

    Verificando el orden de la ejecución de una pagina constructor, render y tercero componentDidMount. Validando la autenticacion con firebase y verificando si el usuario ya esta logueado.

--- app/screens/MyAccount/MyAccount.js
+++ app/screens/MyAccount/MyAccount.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Button } from "react-native-elements";
 
+import * as firebase from "firebase";
+
 export default class MyAccount extends React.Component {
-  /*
+  constructor() {
+    super();
+    console.log("Me ejecuto primero...");
+
+    this.state = {
+      login: false
+    };
+  }
+
   // Funcion que se ejecuta al rederizar la pagina
-  componentDidMount() {
-    console.log(this);
+  async componentDidMount() {
+    console.log("Me ejecuto tercero...");
+    //console.log(this);
     // this contiene toda la informacion del objeto
-    this.props.navigation.navigate("Register");
-  }*/
+    //this.props.navigation.navigate("Register");
+
+    await firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          login: true
+        });
+      }
+    });
+  }
 
   goToScreen = nameScreen => {
     console.log(nameScreen);
@@ -17,19 +36,22 @@ export default class MyAccount extends React.Component {
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>MyAccount Screen!!!</Text>
-        <Button
-          title="Registrar"
-          onPress={() => this.goToScreen("Register")}
-        />
-        <Button
-          title="Login."
-          onPress={() => this.goToScreen("Login")}
-        />
-      </View>
-    );
+    console.log("Me ejecuto segundo...");
+    <Text>MyAccount Screen!!!</Text>;
+    const { login } = this.state;
+    if (login) {
+      return <Text>Estas logueado correctamente....</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Button
+            title="Registrar."
+            onPress={() => this.goToScreen("Register")}
+          />
+          <Button title="Login." onPress={() => this.goToScreen("Login")} />
+        </View>
+      );
+    }
   }
 }
 const styles = StyleSheet.create({
